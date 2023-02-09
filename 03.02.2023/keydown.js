22fdf7be1f693f1f83896abd179d911494bc5c71
DINESH KUMAR <106733790+DINESHYNOT@users.noreply.github.com>
2023-02-09 14:57:31 +0530

    Delete keydown.js

--- 03.02.2023/keydown.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("input").keydown(function(){
-        $(this).css("background-color","silver");
-    });
-    $("input").keyup(function(){
-        $(this).css("background-color","pink");
-    });
-});
